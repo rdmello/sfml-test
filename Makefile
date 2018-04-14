48b974a6f9ec274d3fde8cfea5f24c5b2bea5d49
Rylan Dmello <rylandmello@gmail.com>
2018-04-14 10:00:54 -0400

    Rendering a black window

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 main.run: main.cpp
-	g++ -Wall -Werror -std=c++11 main.cpp -o main.run -I ../SFML-2.4.2/include -L ../SFML-2.4.2/build/lib/ -lsfml-window-s -lsfml-system-s -lX11 -lXrandr -lGL -lpthread -ludev
+	g++ -Wall -Werror -std=c++11 main.cpp -o main.run -I ../SFML-2.4.2/include -L ../SFML-2.4.2/build/lib/ -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lX11 -lXrandr -lGL -lpthread -ludev -ljpeg
 
 clean:
 	rm -f ./*.run
--- main.cpp
+++ main.cpp
@@ -2,22 +2,49 @@
 #include <iostream>
 #include <chrono>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
-void mainloop(sf::Window& w)
+void draw_graphics(sf::RenderWindow& window)
+{
+  // Draw black background
+  window.clear(sf::Color::Black);
+
+  // display (does this use a double-buffer system?)
+  window.display();
+}
+
+void print_framerate(std::chrono::high_resolution_clock::time_point start)
+{
+  using clock = std::chrono::high_resolution_clock;
+  
+  clock::duration elapsed = clock::now() - start;
+  std::chrono::microseconds us_elapsed
+    = std::chrono::duration_cast<std::chrono::microseconds>(elapsed);
+
+  std::cout << "Frame rate: " << 1.0e6 / us_elapsed.count() 
+            << " fps" << std::endl;
+}
+
+void mainloop(sf::RenderWindow& w)
 {
   w.create(sf::VideoMode(1600, 1200), "Test Game");
+  sf::Event event;
 
   while( w.isOpen() )
   {
-    sf::Event event;
+    // time each iteration of the main loop
+    std::chrono::high_resolution_clock::time_point timestart 
+      = std::chrono::high_resolution_clock::now();
 
+    // close Window if close event is detected
     while( w.pollEvent(event) )
     {
-      if (event.type == sf::Event::Closed)
-      {
-        w.close();
-      }
+      if (event.type == sf::Event::Closed) w.close();
     }
+
+    draw_graphics(w);
+
+    print_framerate(timestart);
   }
 }
 
@@ -25,9 +52,8 @@ int main(int argc, char** argv)
 {
   std::cout << "Initializing..." << std::endl;
 
-
   /* Try drawing a simple window */
-  sf::Window window;
+  sf::RenderWindow window;
   mainloop(window); 
   
   std::cout << "Exiting, thanks for playing!" << std::endl;
